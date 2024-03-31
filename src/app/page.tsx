476d74fb8163e32dafb57e3fdd277fba604bc437
mcekov <mtsekov@zohomail.eu>
2024-03-31 14:22:34 +0300

    Add Card for forms and some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import Header from "@/components/ui/header";
 import { SessionProvider } from "next-auth/react";
 import { db } from "@/db";
 import { forms } from "@/db/schema";
+import FormsList from "./forms/FormsList";
 
 export default async function Home() {
   const forms = await db.query.forms.findMany();
 
-  console.log(forms);
-
   return (
     <SessionProvider>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center p-24">
         <SurveyGenerator />
+        <FormsList forms={forms} />
       </main>
     </SessionProvider>
   );
